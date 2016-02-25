a2ff914a8926a30099164c9450775ae12fcc354e
crimaco197 <estigmaster1997@gmail.com>
2016-02-25 00:14:57 -0500

    Added files via upload

---
+++ SIA/src/sia/Teacher.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sia;
+
+/**
+ *
+ * @author estig
+ */
+public class Teacher {
+
+    private String teacherName;
+    private String profession;
+    private int id;
+    private String tutor;
+
+    public String getTeacherName() {
+        return "Teacher's name: " + this.teacherName;
+    }
+
+    public void setTeacherName() {
+    }
+
+    public String getProfession() {
+        return null;
+    }
+
+    public void setProfession() {
+
+    }
+
+    public String getId() {
+        return null;
+    }
+
+    public void setId() {
+
+    }
+
+    public String getTutor() {
+        return null;
+    }
+
+    public void setTutor() {
+
+    }
+}
